538975a23b46e57025d15b4b01e35c4c77ae52ff
damico-kevien <damicokevien@gmail.com>
2026-01-19 18:08:14 +0100

    Scaffolding

---
+++ src/main/java/com/challenge/tax_code_helper/TaxCodeHelperApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.tax_code_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxCodeHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxCodeHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/tax_code_helper/TaxCodeHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.tax_code_helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxCodeHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
